fcf4b589cbc3a866c3ffbf9f9d3f157ba7413449
Leo Tang <leotang634@gmail.com>
2025-03-24 16:05:21 -0400

    Solved AddUpApples

--- Uncategorized/AddUpApples.py
+++ Uncategorized/AddUpApples.py
@@ -1,9 +1,21 @@
 n = int(input())
+counter = 0
 
-def recurse(list, total):
-    if sum(list) == total:
-        print(str(total) + "=" + "+".join(list))
+def recurse(nums, total):
+    count = 0
+    if sum(nums) == total:
+        nums = map(str, nums)
+        print(str(total) + "=" + "+".join(nums))
+        return 1
     else:
-        for i in range(total - sum(list)):
-            recurse(list.append(i), total)
-            # fix this bro it's just recursion
+        for i in range(nums[-1], total - sum(nums) + 1):
+            nums.append(i)
+            count += recurse(nums, total)
+            nums.pop()
+        return count
+
+
+for i in range(1, n // 2 + 1):
+    counter += recurse([i], n)
+
+print("total=" + str(counter))
