5f0aadb7f5fb19b2f3cfe770ba75d68ac2d37c20
Leo Tang <leotang634@gmail.com>
2025-03-26 11:03:28 -0400

    Failed J4 2009, starting Common Divisor

--- CCC/CCC 09/J4Signage.py
+++ CCC/CCC 09/J4Signage.py
@@ -1,34 +1,61 @@
 w = int(input())
 letters = [7, 2, 3, 4, 4, 5]
 words = ["WELCOME", "TO", "CCC", "GOOD", "LUCK", "TODAY"]
+test = 0
 printing = []
 spaces = []
+toremove = []
+finalline = ""
 counter = w
 other = 0
 
 while True:
+    test += 1
     printing = []
     spaces = []
+    toremove = []
+    finalline = ""
     counter = w
     other = 0
     for k, v in enumerate(letters):
-        if counter - v >= 0:
-            counter -= v
-            other += v
-            printing.append(words[k])
+        if k != 0:
+            if counter - v - 1 >= 0:
+                counter -= v + 1
+                other += v
+                printing.append(words[k])
+            else:
+                break
         else:
-            break
+            if counter - v >= 0:
+                counter -= v
+                other += v
+                printing.append(words[k])
+            else:
+                break
     other += len(printing) - 1
     spaces = ["."] * (len(printing) - 1)
-    for i in range(w - other):
-        spaces[(i % len(spaces)) - 1] + "."
+    if w != 7:
+        if len(spaces) == 0:
+            spaces.append((w - len(printing[0])) * ".")
+        else:
+            for i in range(w - other):
+                spaces[(i % len(spaces))] = spaces[(i % len(spaces)) - 1] + "."
+    else:
+        if test != 1:
+            if len(spaces) == 0:
+                spaces.append((w - len(printing[0])) * ".")
+            else:
+                for i in range(w - other):
+                    spaces[(i % len(spaces))] = spaces[(i % len(spaces)) - 1] + "."
     for i in range(len(printing) + len(spaces)):
         if i % 2 == 0:
-            print(printing[i / 2])
+            finalline += printing[int(i / 2)]
         else:
-            print(spaces[(i - 1) / 2])
-    toremove = [ k for k, v in words if k <= len(printing)]
-    for k, v in enumerate(printing):
-        words.remove(v)
-        del letters
-    break
+            finalline += spaces[int((i - 1) / 2)]
+    toremove = reversed([ k for k, v in enumerate(printing) ])
+    for i in toremove:
+        del words[i]
+        del letters[i]
+    print(finalline)
+    if words == []:
+        break
