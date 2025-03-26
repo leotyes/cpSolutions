1e91598d5aeb8b80b985f8b1dcadaac6b7256a47
Leo Tang <leotang634@gmail.com>
2025-03-26 12:49:32 -0400

    No idea why CommonDivisor doesn't work

--- Uncategorized/CommonDivisor.py
+++ Uncategorized/CommonDivisor.py
@@ -1,1 +1,27 @@
-fun gcd
+a, b, k = map(int, input().split())
+c = 1
+f = False
+
+def gcd(a, b):
+    while b != 0:
+        t = b
+        b = a % b
+        a = t
+    return a
+
+firstgcd = gcd(a, b)
+
+if k == 1:
+    print(firstgcd)
+    f = True
+else:
+    for i in range(int((firstgcd ** 0.5) // 1), 0, -1):
+        if firstgcd % i == 0:
+            c += 1
+            if c == k:
+                print(i)
+                f = True
+                break
+
+if not f:
+    print(-1)
