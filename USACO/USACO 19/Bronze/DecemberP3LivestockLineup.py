2d3077e07206de93d1baca9c2e8baadb6bb33985
Leo Tang <leotang634@gmail.com>
2025-04-20 12:51:17 -0400

    Created my own permutation generator for Livestock Lineup

--- USACO/USACO 19/Bronze/DecemberP3LivestockLineup.py
+++ USACO/USACO 19/Bronze/DecemberP3LivestockLineup.py
@@ -1,8 +1,6 @@
-from itertools import permutations
-
 n = int(input())
 cs = {"Buttercup": set(), "Bessie": set(), "Belinda": set(), "Beatrice": set(), "Bella": set(), "Blue": set(), "Betsy": set(), "Sue": set()}
-ps = list(permutations(list(cs.keys())))
+ps = []
 fail = False
 p = []
 
@@ -11,33 +9,45 @@ for i in range(n):
     cs[x].add(y)
     cs[y].add(x)
 
+def milk(l, r):
+    if r == []:
+        ps.append(l)
+    else:
+        for i in r:
+            ll = l.copy()
+            ll.append(i)
+            rr = r.copy()
+            rr.remove(i)
+            milk(ll, rr)
+
+milk([], list(cs.keys()))
+
 for i in ps:
     fail = False
-    l = list(i)
-    for k, v in enumerate(l):
+    for k, v in enumerate(i):
         if len(cs[v]) == 1:
             if k == 0:
-                if l[k + 1] != list(cs[v])[0]:
+                if i[k + 1] != list(cs[v])[0]:
                     fail = True
-            elif k == len(l) - 1:
-                if l[k - 1] != list(cs[v])[0]:
+            elif k == len(i) - 1:
+                if i[k - 1] != list(cs[v])[0]:
                     fail = True
             else:
-                if l[k + 1] != list(cs[v])[0] and l[k - 1] != list(cs[v])[0]:
+                if i[k + 1] != list(cs[v])[0] and i[k - 1] != list(cs[v])[0]:
                     fail = True
         elif len(cs[v]) == 2:
             if k == 0:
                 fail = True
-            elif k == len(l) - 1:
+            elif k == len(i) - 1:
                 fail = True
             else:
-                if l[k + 1] != list(cs[v])[0] or l[k - 1] != list(cs[v])[1]:
-                    if l[k + 1] != list(cs[v])[1] or l[k - 1] != list(cs[v])[0]:
+                if i[k + 1] != list(cs[v])[0] or i[k - 1] != list(cs[v])[1]:
+                    if i[k + 1] != list(cs[v])[1] or i[k - 1] != list(cs[v])[0]:
                         fail = True
         if fail:
             break
     if not fail:
-        p.append(l)
+        p.append(i)
 
 for i in sorted(p)[0]:
     print(i)
