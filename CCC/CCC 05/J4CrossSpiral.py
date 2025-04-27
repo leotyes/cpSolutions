84a6c3cd5624a4b3c330cad27f725be7b6c2bd15
Leo Tang <leotang634@gmail.com>
2025-04-27 13:31:11 -0400

    Solved J4 Cross Spiral

--- CCC/CCC 05/J4CrossSpiral.py
+++ CCC/CCC 05/J4CrossSpiral.py
@@ -12,22 +12,108 @@ for i in range(y):
             if j < cx or x - j - 1< cx:
                 xy[i][j] = "x"
 
-def walk(px, py):
-    if px == 3 and py == 6:
-        st[1] = True
+# def walk(px, py):
+#     if px == 3 and py == 6:
+#         st[1] = True
+#     if st[0] == s:
+#         print(px + 1)
+#         print(py + 1)
+#     else:
+#         st[0] += 1
+#         xy[py][px] = "x"
+#         if px != x - 1 and xy[py][px + 1] == "o":
+#             walk(px + 1, py)
+#         elif py != y - 1 and xy[py + 1][px] == "o":
+#             walk(px, py + 1)
+#         elif px != 0 and xy[py][px - 1] == "o":
+#             walk(px - 1, py)
+#         elif py != 0 and xy[py - 1][px] == "o":
+#             walk(px, py - 1)
+#
+# walk(cx, 0)
+
+def walk(px, py, d):
     if st[0] == s:
         print(px + 1)
         print(py + 1)
     else:
         st[0] += 1
         xy[py][px] = "x"
-        if px != x - 1 and xy[py][px + 1] == "o":
-            walk(px + 1, py)
-        elif py != y - 1 and xy[py + 1][px] == "o":
-            walk(px, py + 1)
-        elif px != 0 and xy[py][px - 1] == "o":
-            walk(px - 1, py)
-        elif py != 0 and xy[py - 1][px] == "o":
-            walk(px, py - 1)
+        if d % 12 == 1 or d % 12 == 3 or d % 12 == 11:
+            if d % 12 == 11:
+                if py != 0 and xy[py - 1][px] == "o":
+                    walk(px, py - 1, d + 1)
+                else:
+                    if xy[py][px + 1] == "x":
+                        print(px + 1)
+                        print(py + 1)
+                    else:
+                        walk(px + 1, py, d)
+            else:
+                if px != x - 1 and xy[py][px + 1] == "o":
+                    walk(px + 1, py, d)
+                else:
+                    if xy[py + 1][px] == "x":
+                        print(px + 1)
+                        print(py + 1)
+                    else:
+                        walk(px, py + 1, d + 1)
+        elif d % 12 == 2 or d % 12 == 4 or d % 12 == 6:
+            if d % 12 == 2:
+                if px != x - 1 and xy[py][px + 1] == "o":
+                    walk(px + 1, py, d + 1)
+                else:
+                    if xy[py + 1][px] == "x":
+                        print(px + 1)
+                        print(py + 1)
+                    else:
+                        walk(px, py + 1, d)
+            else:
+                if py != y - 1 and xy[py + 1][px] == "o":
+                    walk(px, py + 1, d)
+                else:
+                    if xy[py][px - 1] == "x":
+                        print(px + 1)
+                        print(py + 1)
+                    else:
+                        walk(px - 1, py, d + 1)
+        elif d % 12 == 5 or d % 12 == 7 or d % 12 == 9:
+            if d % 12 == 5:
+                if py != y - 1 and xy[py + 1][px] == "o":
+                    walk(px, py + 1, d + 1)
+                else:
+                    if xy[py][px - 1] == "x":
+                        print(px + 1)
+                        print(py + 1)
+                    else:
+                        walk(px - 1, py, d)
+            else:
+                if py != 0 and xy[py][px - 1] == "o":
+                    walk(px - 1, py, d)
+                else:
+                    if xy[py - 1][px] == "x":
+                        print(px + 1)
+                        print(py + 1)
+                    else:
+                        walk(px, py - 1, d + 1)
+        elif d % 12 == 8 or d % 12 == 10 or d % 12 == 0:
+            if d % 12 == 8:
+                if px != 0 and xy[py][px - 1] == "o":
+                    walk(px - 1, py, d + 1)
+                else:
+                    if xy[py - 1][px] == "x":
+                        print(px + 1)
+                        print(py + 1)
+                    else:
+                        walk(px, py - 1, d)
+            else:
+                if py != 0 and xy[py - 1][px] == "o":
+                    walk(px, py - 1, d)
+                else:
+                    if xy[py][px + 1] == "x":
+                        print(px + 1)
+                        print(py + 1)
+                    else:
+                        walk(px + 1, py, d + 1)
 
-walk(cx, 0)
+walk(cx, 0, 1)
