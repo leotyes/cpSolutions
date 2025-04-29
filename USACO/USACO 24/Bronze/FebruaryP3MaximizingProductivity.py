89690a8c98ab052f5109e1f5c0791903e4fea461
Leo Tang <leotang634@gmail.com>
2025-04-28 21:19:49 -0400

    Solved P3 Maximizing Productivity

--- USACO/USACO 24/Bronze/FebruaryP3MaximizingProductivity.py
+++ USACO/USACO 24/Bronze/FebruaryP3MaximizingProductivity.py
@@ -2,3 +2,15 @@ import sys
 input = sys.stdin.readline
 
 n, q = map(int, input().strip().split())
+c = list(map(int, input().strip().split()))
+t = list(map(int, input().strip().split()))
+ct = sorted([ a - b for a, b in zip(c, t) ], reverse=True)
+cc = 0
+
+for i in range(q):
+    cc = 0
+    v, s = map(int, input().strip().split())
+    if ct[v - 1] > s:
+        print("YES")
+    else:
+        print("NO")
