44b1321038f3ae6be3c6f050cc41701cf5bc4c84
Leo Tang <leotang634@gmail.com>
2025-05-02 13:19:01 -0400

    Finally solved MaxGCD

--- Uncategorized/MaxGCD.py
+++ Uncategorized/MaxGCD.py
@@ -29,50 +29,14 @@ else:
     o = gcd(ns[-1], ns[-2])
     s.append(o)
 
-    for i in range(n - 1, 1, -1):
+    for i in range(n - 2, 0, -1):
         o = gcd(o, ns[i - 1])
         s.append(o)
 
     m = p[-2]
-    m = max(gcd(ns[0], s[-2]), m)
+    m = max(s[-2], m)
 
-    for i in range(2, n - 1):
-        m = max(m, gcd(p[i - 1], s[i - 1]))
+    for i in range(1, n - 1):
+        m = max(m, gcd(p[i - 1], s[-2 - i]))
 
-    m = max(gcd(ns[-1], p[-2]), m)
-    m = max(m, s[-2])
-
-    if m in ns:
-        print(sorted(ns)[1])
-    else:
-        print(m)
-
-
-# def main():
-#     input = sys.stdin.readline
-#     n = int(input())
-#     a = list(map(int, input().split()))
-#     # Prefix-GCD: prefix[i] = gcd(a[0],…,a[i])
-#     prefix = [0]*n
-#     prefix[0] = a[0]
-#     for i in range(1, n):
-#         prefix[i] = gcd(prefix[i-1], a[i])
-#     # Suffix-GCD:  suffix[i] = gcd(a[i],…,a[n-1])
-#     suffix = [0]*n
-#     suffix[n-1] = a[n-1]
-#     for i in range(n-2, -1, -1):
-#         suffix[i] = gcd(suffix[i+1], a[i])
-#
-#     answer = 0
-#     for i in range(n):
-#         if i == 0:
-#             # remove a[0], use suffix[1]
-#             answer = max(answer, suffix[1])
-#         elif i == n-1:
-#             # remove a[n-1], use prefix[n-2]
-#             answer = max(answer, prefix[n-2])
-#         else:
-#             # remove a[i], combine prefix[i-1] and suffix[i+1]
-#             answer = max(answer, gcd(prefix[i-1], suffix[i+1]))
-#
-#     print(answer)
+    print(m)
